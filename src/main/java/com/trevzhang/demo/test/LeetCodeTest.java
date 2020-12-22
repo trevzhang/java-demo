d32995b2b5b2982b28cbc88f5afddf31d188a780
trevzhang <trevzhang@163.com>
2020-12-22 14:27:32 +0800

    LeetCodeTest

---
+++ src/main/java/com/trevzhang/demo/test/LeetCodeTest.java
@@ -0,0 +1,19 @@
+package com.trevzhang.demo.test;
+
+/**
+ * 用于调试力扣题解
+ *
+ * @author Trevor Zhang
+ * @since 2020/12/22
+ */
+public class LeetCodeTest {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+// ----------代码粘贴区---------
+class Solution {
+
+}
