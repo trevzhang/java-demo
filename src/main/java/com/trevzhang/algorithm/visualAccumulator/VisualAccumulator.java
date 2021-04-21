c78c2a0f1f69fd3abbcd0bb74677a074c0a060f0
春火 <zhangchunguang.zcg@alibaba-inc.com>
2021-04-21 17:09:14 +0800

    test

---
+++ src/main/java/com/trevzhang/algorithm/visualAccumulator/VisualAccumulator.java
@@ -0,0 +1,32 @@
+package com.trevzhang.algorithm.visualAccumulator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 春火
+ * @since 2021/4/15 4:08 下午
+ */
+public class VisualAccumulator {
+    private double total;
+    private int N;
+
+    interface Bean {
+
+    }
+
+    class SubBean implements Bean {
+
+    }
+
+    public void test(String[] args) {
+        Bean bean = new SubBean();
+        Bean bean1 = new SubBean();
+        SubBean bean2 = new SubBean();
+        List<Bean> list = new ArrayList<>();
+        list.add(bean);
+        list.add(bean1);
+        list.add(bean2);
+        System.out.println("test");
+    }
+}
