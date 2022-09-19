0cd8437425c72d6f49ef0935cc32ccf77899a9de
trevzhang <trevzhang@163.com>
2022-09-19 11:11:06 +0800

    update DateTest.java

--- src/test/java/com/trevzhang/demo/test/DateTest.java
+++ src/test/java/com/trevzhang/demo/test/DateTest.java
@@ -1,20 +1,19 @@
 package com.trevzhang.demo.test;
 
 import java.util.Date;
+import org.junit.Test;
 
 /**
+ * 日期测试
+ *
  * @author 春火
  * @since 2021/1/28 1:57 下午
  */
 public class DateTest {
-    public static void main(String[] args) {
-        int i, j;
-        i = 3100;
-        j = 3300;
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int k = i; k < j; k++) {
-            stringBuilder.append(k).append(",");
-        }
-        System.out.println(stringBuilder.toString());
+
+    @Test
+    public void testStringBuilder() {
+        Date testDate = new Date(116, 3, 5);
+        System.out.println(testDate);
     }
 }
