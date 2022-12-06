e502cc03061cfc208375f2917c3ca507cf4785d8
trevzhang <trevzhang@163.com>
2022-12-06 15:05:54 +0800

    docs: update EnumTest.java

--- src/test/java/com/trevzhang/demo/test/EnumTest.java
+++ src/test/java/com/trevzhang/demo/test/EnumTest.java
@@ -10,10 +10,20 @@ public class EnumTest {
 
     /**
      * Enum.valueOf(Class,String) 返回指定枚举类型中匹配的枚举
+     * 注意：如果没有匹配到的枚举类型，会抛出java.lang.IllegalArgumentException异常
      */
     @Test
     public void testEnumValueOf() {
         AnimalEnum animalEnum = Enum.valueOf(AnimalEnum.class, "TIGER");
+        System.out.println(animalEnum);
+    }
+
+    /**
+     * AnimalEnum.valueOf(String) 等价于 Enum.valueOf(AnimalEnum.class, String)
+     */
+    @Test
+    public void testEnumValueOf2() {
+        AnimalEnum animalEnum = AnimalEnum.valueOf("TIGER");
         System.out.println(animalEnum);
     }
 
