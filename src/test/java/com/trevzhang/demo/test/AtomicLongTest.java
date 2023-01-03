4649351453874b855bd07c9e4ad43acdafd40f79
trevzhang <trevzhang@163.com>
2023-01-03 14:25:53 +0800

    feat: add AtomicLongTest.java

--- src/test/java/com/trevzhang/demo/test/AtomicLongTest.java
+++ src/test/java/com/trevzhang/demo/test/AtomicLongTest.java
@@ -1,6 +1,42 @@
-package com.trevzhang.demo.test;/**
- *
+package com.trevzhang.demo.test;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
+/**
  * @author zhangchunguang.zcg
  * @since 2023/1/3 2:18 PM
- */public class AtomicLongTest {
+ */
+@Slf4j
+public class AtomicLongTest {
+
+    public static void main(String[] args) {
+        Count count = new Count();
+        ExecutorService service = Executors.newCachedThreadPool();
+        // 多线程并发CAS 1000次
+        for (int i = 0; i < 1000; i++) {
+            service.execute(count::increase);
+        }
+        service.shutdown();
+        try {
+            // 同步等待所有线程结束
+            boolean sync = service.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("End count :{}", count.getCount());
+    }
+
+    @Data
+    static class Count {
+        private Long count = 0L;
+
+        public void increase() {
+            log.info("count {}", count++);
+        }
+    }
 }
