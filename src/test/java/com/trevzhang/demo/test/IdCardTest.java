4bb786043ff65d0f90ad6d2147219fc03ec518c9
trevzhang <trevzhang@163.com>
2023-01-03 14:16:26 +0800

    add IdCardTest.java

--- src/test/java/com/trevzhang/demo/test/IdCardTest.java
+++ src/test/java/com/trevzhang/demo/test/IdCardTest.java
@@ -1,6 +1,56 @@
-package com.trevzhang.demo.test;/**
- *
+package com.trevzhang.demo.test;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
  * @author zhangchunguang.zcg
  * @since 2022/12/20 12:39 PM
- */public class IdCardTest {
+ */
+public class IdCardTest {
+
+    public static void main(String[] args) throws IOException {
+        String url = "https://puhui.shumaidata.com/id_card/check/puhui";
+        String appCode = "";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("idcard", "12345678");
+        params.put("name", "张三");
+
+        String result = get(appCode, url, params);
+        System.out.println(result);
+    }
+
+    /**
+     * 用到的HTTP工具包：okhttp 3.13.1
+     * <dependency>
+     * <groupId>com.squareup.okhttp3</groupId>
+     * <artifactId>okhttp</artifactId>
+     * <version>3.13.1</version>
+     * </dependency>
+     */
+    public static String get(String appCode, String url, Map<String, String> params) throws IOException {
+        url = url + buildRequestUrl(params);
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        Request request = new Request.Builder().url(url).addHeader("Authorization", "APPCODE " + appCode).build();
+        Response response = client.newCall(request).execute();
+        System.out.println("返回状态码" + response.code() + ",message:" + response.message());
+        String result = response.body().string();
+        return result;
+    }
+
+    public static String buildRequestUrl(Map<String, String> params) {
+        StringBuilder url = new StringBuilder("?");
+        Iterator<String> it = params.keySet().iterator();
+        while (it.hasNext()) {
+            String key = it.next();
+            url.append(key).append("=").append(params.get(key)).append("&");
+        }
+        return url.toString().substring(0, url.length() - 1);
+    }
 }
