20bbecbbe7ef0f16dad86c6105be1878f4899eff
trevzhang <trevzhang@163.com>
2023-02-09 17:41:14 +0800

    :white_check_mark: add JSONPath.eval() tests

--- src/test/java/com/trevzhang/demo/test/JSONTest.java
+++ src/test/java/com/trevzhang/demo/test/JSONTest.java
@@ -1,7 +1,9 @@
 package com.trevzhang.demo.test;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
 import com.alibaba.fastjson.TypeReference;
 import java.util.Map;
 import org.junit.Test;
@@ -11,6 +13,62 @@ import org.junit.Test;
  * @since 2022/4/24 4:42 PM
  */
 public class JSONTest {
+
+    @Test
+    public void testJsonPathEval() {
+        String jsonDemo = "{\n"
+            + "    \"status\": 200,\n"
+            + "    \"msg\": \"成功\",\n"
+            + "    \"data\":\n"
+            + "    {\n"
+            + "        \"name\": \"Smith\",\n"
+            + "        \"age\": 15,\n"
+            + "        \"address\": \"青岛\"\n"
+            + "    }\n"
+            + "}";
+
+        String jsonArrayDemo = "[\n"
+            + "        {\n"
+            + "            \"name\": \"Lucy\",\n"
+            + "            \"age\": 10,\n"
+            + "            \"address\": \"上海\"\n"
+            + "        },\n"
+            + "        {\n"
+            + "            \"name\": \"Tom\",\n"
+            + "            \"age\": 12,\n"
+            + "            \"address\": \"北京\"\n"
+            + "        },\n"
+            + "        {\n"
+            + "            \"name\": \"Smith\",\n"
+            + "            \"age\": 15,\n"
+            + "            \"address\": \"青岛\"\n"
+            + "        },\n"
+            + "        {\n"
+            + "            \"name\": \"Jerry\",\n"
+            + "            \"age\": 21,\n"
+            + "            \"address\": \"秦皇岛\"\n"
+            + "        }\n"
+            + "    ]\n";
+        JSONObject jsonObject = JSONObject.parseObject(jsonDemo);
+        String path = "$.data.name";
+        String response = (String) JSONPath.eval(jsonObject, path);
+        System.out.printf("路径：%s，单个提取：%s\n", path, response);
+
+        JSONArray jsonArray = JSON.parseArray(jsonArrayDemo);
+        String arrayPath = "[1].name";
+        String responseArray = (String)JSONPath.eval(jsonArray, arrayPath);
+        System.out.printf("路径：%s，单个提取：%s\n", arrayPath, responseArray);
+    }
+
+    @Test
+    public void testGetBoolean() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("canDawnBook", false);
+        System.out.println(jsonObject);
+
+        Boolean canDawnBook = jsonObject.getBoolean("canDawnBook");
+        System.out.println(canDawnBook);
+    }
 
     @Test
     public void testJSONClassParse() {
@@ -27,7 +85,7 @@ public class JSONTest {
 
         // @type为JSONObject，反序列化为Map<String,String>成功
         String jsonStr3 = "{\"@type\":\"com.alibaba.fastjson.JSONObject\",\"signAlipayAgent\":false}";
-        Map<String,String> map3 = JSON.parseObject(jsonStr3, new TypeReference<Map<String, String>>() {
+        Map<String, String> map3 = JSON.parseObject(jsonStr3, new TypeReference<Map<String, String>>() {
         });
         System.out.println(map3);
     }
