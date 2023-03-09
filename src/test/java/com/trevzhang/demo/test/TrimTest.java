ea0f495abe4ab47d499f3e23ec5c99a81d21a14f
trevzhang <trevzhang@163.com>
2023-03-09 16:48:37 +0800

    :white_check_mark: test StringUtils.trim

---
+++ src/test/java/com/trevzhang/demo/test/TrimTest.java
@@ -0,0 +1,19 @@
+package com.trevzhang.demo.test;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+/**
+ * @author zhangchunguang.zcg
+ * @since 2023/3/9 16:45
+ */
+public class TrimTest {
+
+    @Test
+    public void testTrim() {
+        String testStr = " 泰国 ";
+        String trimed = StringUtils.trim(testStr);
+        System.out.println("begin{"+testStr+"}end");
+        System.out.println("begin{"+trimed+"}end");
+    }
+}
