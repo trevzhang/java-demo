fbfab18f57bed9b922f360e92d8ecb72277385d9
trevzhang <trevzhang@163.com>
2023-03-30 17:39:45 +0800

    feat: update CompareTest

--- src/test/java/com/trevzhang/demo/test/CompareTest.java
+++ src/test/java/com/trevzhang/demo/test/CompareTest.java
@@ -2,11 +2,13 @@ package com.trevzhang.demo.test;
 
 import cn.hutool.core.date.DateField;
 import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSON;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import lombok.Data;
+import org.junit.Test;
 
 /**
  * 列表排序工具
@@ -15,6 +17,21 @@ import lombok.Data;
  * @since 2022/3/2 3:08 PM
  */
 public class CompareTest {
+
+    @Test
+    public void test() {
+        List<String> list = new ArrayList<>();
+        list.add("2022-03-10");
+        list.add("2022-03-27");
+        list.add("2022-06-27");
+        list.add("2022-04-01");
+        list.add("2022-05-12");
+
+        System.out.println(JSON.toJSONString(list));
+    }
+
+
+
     public static void main(String[] args) {
         List<CompareBean> rawList = generateList();
         System.out.println(rawList);
