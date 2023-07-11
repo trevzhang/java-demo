e08933ecbbc9ae3e63c8e468c14dcda68b9de906
trevzhang <trevzhang@163.com>
2023-07-11 19:13:24 +0800

    A

--- src/test/java/com/trevzhang/demo/test/ABADemo.java
+++ src/test/java/com/trevzhang/demo/test/ABADemo.java
@@ -12,7 +12,7 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 public class ABADemo {
     private static AtomicReference<Integer> atomicReference = new AtomicReference<>(100);
     private static AtomicStampedReference<Integer> stampedReference = new AtomicStampedReference<>(100, 1);
-
+//A
     public static void main(String[] args) {
         System.out.println("===以下是ABA问题的产生===");
         new Thread(() -> {
