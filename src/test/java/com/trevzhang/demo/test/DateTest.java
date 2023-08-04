17f4f6a2a4184b33b34960e916a1b3d32b8f5526
trevzhang <trevzhang@163.com>
2023-08-04 10:57:25 +0800

    test: Test Date Ceiling

--- src/test/java/com/trevzhang/demo/test/DateTest.java
+++ src/test/java/com/trevzhang/demo/test/DateTest.java
@@ -1,6 +1,9 @@
 package com.trevzhang.demo.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 
 /**
@@ -11,6 +14,13 @@ import org.junit.Test;
  */
 public class DateTest {
 
+    @Test
+    public void testDateCeiling() {
+        Date now = new Date();
+        Date dateAfterHalfYear = DateUtils.ceiling(DateUtils.addDays(now, 180), Calendar.DATE);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(dateAfterHalfYear));
+    }
     @Test
     public void testStringBuilder() {
         Date testDate = new Date(116, 3, 5);
