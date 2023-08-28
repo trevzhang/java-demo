44d742269e62f52e13ef047c5ad3621b6e2a30be
trevzhang <trevzhang@163.com>
2023-08-28 11:33:18 +0800

    :sparkles: Add MaskUtilTest

---
+++ src/main/java/com/trevzhang/demo/util/MaskUtil.java
@@ -0,0 +1,6 @@
+package com.trevzhang.demo.util;/**
+ *
+ * @author zhangchunguang.zcg
+ * @since 2023/8/28 11:26
+ */public class MaskUtil {
+}
---
+++ src/test/java/com/trevzhang/demo/test/MaskUtilTest.java
@@ -0,0 +1,6 @@
+package com.trevzhang.demo.test;/**
+ *
+ * @author zhangchunguang.zcg
+ * @since 2023/8/28 11:26
+ */public class MaskUtil {
+}
